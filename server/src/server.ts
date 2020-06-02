1d5e076d667a7d51b49ced1dce4bd7ed0422bbad
Andressa Schinoff <andressaschinoff@gmail.com>
2020-06-02 19:54:49 -0300

    creat react app template

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,3 +1,12 @@
 import express from 'express';
 
 const app = express();
+
+app.get('/users', (request, response) => {
+  // JSON
+  response.json(['Diego', 'Cleiton', 'Robson', 'Daniel', 'Fernando']);
+});
+
+app.listen(3333, () => {
+  console.log('app is running');
+});
